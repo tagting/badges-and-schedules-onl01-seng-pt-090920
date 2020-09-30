8173a54b3af87526d6da4a4b577fa98f095877cf
Ting Ting Zheng <ttzhenng@gmail.com>
2020-09-30 19:35:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def badge_maker(attendees)
+  return "Hello, my name is #{name}."
+end
+
+def assign_rooms(attendees)
+  attendees.each_with_index{|val,idx| "Hello,#{val}! You'll be assigned to room #{idx + 1}!"}
+end
+
+def printer
